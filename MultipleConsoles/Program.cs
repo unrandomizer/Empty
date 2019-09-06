2a523dd2d19cf85d83b86e498f71d400aa00736d
Rostislav <therostikru@gmail.com>
2019-09-06 04:10:50 +0200

    Добавьте файлы проекта.

---
+++ MultipleConsoles/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+
+// 
+// Why there is no new cmd window?
+//
+namespace MultipleConsoles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProcessStartInfo psi = new ProcessStartInfo("cmd.exe")
+            {
+                WindowStyle = ProcessWindowStyle.Normal,
+                RedirectStandardError = true,
+                RedirectStandardInput = true,
+                RedirectStandardOutput = true,
+                UseShellExecute = false
+            };
+            Process p = Process.Start(psi);
+            StreamWriter sw = p.StandardInput;
+            StreamReader sr = p.StandardOutput;
+            sw.WriteLine("Hello world @new_console");
+            Console.WriteLine("Hello world @native_console");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SteamFees/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace SteamFees
+{
+    class Program
+    {
+        static void Main()
+        {
+            for (int input = 1; input < 105; input++)
+            {
+                int receivedAmount = GetPriceToSend(input);
+                int correctedInput = GetThirdPartyPrice(receivedAmount);
+                string str = $"You receive {receivedAmount / 100f} / Buyer pays {correctedInput / 100f}";
+                Console.WriteLine(str);
+            }
+            Console.WriteLine("Hello World!");
+        }
+        public static int GetThirdPartyPrice(int priceToSend)
+        {
+            int steamFee = (int)Math.Floor(Math.Max(priceToSend * 0.05f, 1));
+            int publisherFee = (int)Math.Floor(Math.Max(priceToSend * 0.1f, 1));
+            int thirdPartyPrice = priceToSend + steamFee + publisherFee;
+            return thirdPartyPrice;
+        }
+        public static int GetPriceToSend(int thirdPartyPrice)
+        {
+            int steamFee = Math.Max((int)Math.Floor(thirdPartyPrice - thirdPartyPrice / 1.05f), 1);
+            int publisherFee = Math.Max((int)Math.Floor(thirdPartyPrice - thirdPartyPrice / 1.1f), 1);
+            int priceToSend = Math.Max(thirdPartyPrice - steamFee - publisherFee, 1);
+            return priceToSend;
+        }
+    }
+}
